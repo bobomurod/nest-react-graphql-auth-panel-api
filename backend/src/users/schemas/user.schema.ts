255c34ae09dd0e4aab544abdca955dcf972a7031
bobomurod <bobomurod.boboev@yandex.ru>
2020-12-10 14:22:01 +0500

    prepare schema init

--- backend/src/users/schemas/user.schema.ts
+++ backend/src/users/schemas/user.schema.ts
@@ -1,4 +1,18 @@
-export class UserSchema {
-    @Prop
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document } from 'mongoose'
 
+export type UserDocument = UserSchema & Document
+
+export class UserSchema {
+  @Prop()
+  username: string;
+
+  @Prop()
+  login: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  phone: string;
 }
