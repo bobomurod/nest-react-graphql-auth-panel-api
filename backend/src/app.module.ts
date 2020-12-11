f8cc7306e6798c34b8fef39cae574ec77a1b20c4
bobomurod <bobomurod.boboev@yandex.ru>
2020-12-11 08:46:31 +0500

    env files setup, import UserModule in main module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosController } from './todos/todos.controller';
 import { TodosService } from './todos/todos.service';
+import {ConfigModule} from "@nestjs/config";
+import {UsersModule} from "./users/users.module";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
   imports: [
+      ConfigModule.forRoot(),
+      UsersModule,
+      MongooseModule.forRoot()
     GraphQLModule.forRoot({
       autoSchemaFile: true,
   }),
--- backend/src/todos/todos.service.ts
+++ backend/src/todos/todos.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTodosDto } from './dto/create-todos.dto';
 
 @Injectable()
 export class TodosService {
