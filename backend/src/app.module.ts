5e3f6d68a5fbaa36eda347b15326bb77d44f4218
bobomurod <bobomurod.boboev@yandex.ru>
2020-12-12 06:45:36 +0500

    modules refactoring (moved to folder)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodosController } from './todos/todos.controller';
-import { TodosService } from './todos/todos.service';
+import { TodosController } from './modules/todos/todos.controller';
+import { TodosService } from './modules/todos/todos.service';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
