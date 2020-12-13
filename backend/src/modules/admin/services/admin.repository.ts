65fc46e7e31fd04d9572b6a17ed8e526ac8ee2e6
bobomurod <bobomurod.boboev@yandex.ru>
2020-12-13 07:39:53 +0500

    admin findOne

--- backend/src/modules/admin/services/admin.repository.ts
+++ backend/src/modules/admin/services/admin.repository.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AdminRepository {}
+export class AdminRepository {
+    async findOne(): Promise<> {
+        return;
+    }
+}
