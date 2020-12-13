008b2a5e3bfeb7e143960b49c439bca02972e5c9
bobomurod <bobomurod.boboev@yandex.ru>
2020-12-13 06:56:19 +0500

    admin service

--- backend/src/modules/admin/admin.module.ts
+++ backend/src/modules/admin/admin.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AdminService } from './services/admin.service';
 
-@Module({})
+@Module({
+  providers: [AdminService]
+})
 export class AdminModule {}
