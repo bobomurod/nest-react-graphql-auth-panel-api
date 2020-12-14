007683231ac742be413bdc2b55875eb28aa8abf6
bobomurod <bobomurod.boboev@yandex.ru>
2020-12-14 07:45:59 +0500

    admin model

---
+++ backend/src/modules/admin/model/admin.ts
@@ -0,0 +1,5 @@
+export class Admin {
+  id: string;
+  login: string;
+  password: string;
+}
--- backend/src/modules/admin/services/admin.repository.ts
+++ backend/src/modules/admin/services/admin.repository.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Admin } from '../model/admin';
 
 @Injectable()
 export class AdminRepository {
-    async findOne(): Promise<> {
-        return;
-    }
+  async findOne(): Promise<Admin | undefined> {
+    return null;
+  }
 }
